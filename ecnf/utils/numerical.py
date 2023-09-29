925890637efc42df2a03c78130a96bb7f0f43d88
lollcat <laurencemidgley@gmail.com>
2023-09-29 13:22:27 +0100

    feat: copy and paste utils

---
+++ ecnf/utils/numerical.py
@@ -0,0 +1,37 @@
+import jax.numpy as jnp
+import chex
+import jax
+
+def safe_norm(x: chex.Array, axis: int = None, keepdims: bool = False) -> jnp.ndarray:
+    """nan-safe norm. Copied from mace-jax"""
+    x2 = jnp.sum(x**2, axis=axis, keepdims=keepdims)
+    return jnp.where(x2 == 0, 1, x2) ** 0.5
+
+def vector_rejection(a: chex.Array, b: chex.Array) -> chex.Array:
+    chex.assert_rank(a, 1)
+    chex.assert_equal_shape((a, b))
+    vector_proj = b * jnp.dot(a, b) / jnp.dot(b, b)
+    return a - vector_proj
+
+def rotate_3d(x: chex.Array, theta: chex.Array, phi: chex.Array) -> chex.Array:
+    chex.assert_shape(theta, ())
+    chex.assert_shape(x, (3,))
+    rotation_matrix_1 = jnp.array(
+        [[jnp.cos(theta), -jnp.sin(theta), 0],
+         [jnp.sin(theta), jnp.cos(theta), 0],
+         [0,              0,              1]]
+    )
+    rotation_matrix_2 = jnp.array([
+        [1, 0, 0],
+        [0, jnp.cos(phi), -jnp.sin(phi)],
+        [0, jnp.sin(phi), jnp.cos(phi)],
+         ])
+    x = jnp.matmul(rotation_matrix_1, x)
+    x = jnp.matmul(rotation_matrix_2, x)
+    return x
+
+def get_leading_axis_tree(tree: chex.ArrayTree, n_dims: int = 1) -> chex.Shape:
+    flat_tree, _ = jax.tree_util.tree_flatten(tree)
+    leading_shape = flat_tree[0].shape[:n_dims]
+    chex.assert_tree_shape_prefix(tree, leading_shape)
+    return leading_shape
