cd91e249c61d8f705f2531c3937c16a5ccbd2169
lollcat <laurencemidgley@gmail.com>
2023-11-02 20:17:46 +0000

    feat: eval bug fix

--- ecnf/setup_training.py
+++ ecnf/setup_training.py
@@ -6,7 +6,6 @@ from functools import partial
 
 import jax.numpy as jnp
 import jax
-import chex
 import optax
 import wandb
 import chex
@@ -14,6 +13,7 @@ import matplotlib.pyplot as plt
 from jax.flatten_util import ravel_pytree
 from omegaconf import DictConfig
 
+from ecnf.utils.numerical import maybe_masked_mean
 from ecnf.targets.data import FullGraphSample
 from ecnf.cnf.core import FlowMatchingCNF
 from ecnf.cnf.build_cnf import build_cnf
@@ -200,9 +200,9 @@ def setup_training(
 
         info = {}
         info.update(
-            test_log_lik=jnp.mean(log_q),
-            test_log_prob_base=jnp.mean(log_prob_base),
-            test_delta_log_lik=jnp.mean(delta_log_lik),
+            test_log_lik=maybe_masked_mean(log_q, mask),
+            test_log_prob_base=maybe_masked_mean(log_prob_base, mask),
+            test_delta_log_lik=maybe_masked_mean(delta_log_lik, mask),
         )
 
         if target_log_prob_fn is not None:
